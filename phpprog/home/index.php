f21518309fdae2252def0ebe0bb018e73c35224a
Dewni Seneviratne <87601733+DewniSeneviratne@users.noreply.github.com>
2022-02-23 22:26:08 +0530

    Update index.php

--- phpprog/home/index.php
+++ phpprog/home/index.php
@@ -1,1 +1,178 @@
+<?php
+?>
+<html>
+<head>
+<a name="top"></a>
+<title>Sedna</title>
+<style>
+@font-face{
+src:url("FontsFree-Net-vanitas.ttf");
+font-family:raleway;
+font-family:ø;
+font-family:roboto;
 
+}
+</style>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@900&display=swap" rel="stylesheet">
+
+<!- font roboto -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+
+<!- font Raleway -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
+
+<link rel="stylesheet"
+type="text/css"
+href="webstyles.css">
+
+
+</head>
+<body>
+<div class="menu">
+<a href="http://localhost/phpprog/home/index.php">HOME</a>
+<a href="http://localhost/phpprog/finalfeed.php">FEED</a>
+<a href="http://localhost/phpprog/contribution.php">CONTRIBUTION</a>
+<a href="http://localhost/phpprog/home/profile.php">PROFILE</a>
+<a href="http://localhost/phpprog/home/login.html">LOGIN</a>
+<span class="logo">
+<a id="tw" href="https://twitter.com/Sedna2022"><img src="tw.png" height="45px" width="47px"></a>
+<a id="fb" href="https://www.facebook.com/profile.php?id=100077396950173"><img src="fb.png" height="55px" width="55px"></a>
+<a id="ig" href="https://instagram.com/sedn_a_?utm_medium=copy_link"><img src="ig.png" height="50px" width="68px"></a>
+</span>
+</div>
+
+<div class="container">
+ <span class="text1"> SEDNA</span>
+ <span class="text2"> MAKE THE CHANGE </span>
+</div>
+
+<div class="Img"></div>
+
+<div class="block1">
+<div class="box1">
+<span class="gr">WANT TO BE A LEGEND?</span>
+</br></br></br></br>
+<p class="para1">
+Let's protect the lives below water.
+You don't need a team or lot of equipments, all you need is a good spirit and determination.
+</br></br>C'mon! be the doctor to heal the ocean. 
+</p>
+</div>
+</div>
+<div class="Img2"></div>
+<div class="block2">
+<h1 class="obj"> OBJECTIVE</h1>
+<p class="para2">Here what we expect is contributing to acheive the 14th SDG goal: Life below water. This would be the platform of the century to upload
+both positive and negative side of coastal areas. Anyone can help us by
+<ul class="list1">
+<li>Posting to the feed</li>
+<li>Making people aware</li>
+<li>Volunteering</li>
+<li>Donating</li>
+<li>Sharing</li>
+</ul>
+</p>
+</div>
+<div class="image3"></div>
+<div class="aware">
+<span class="aw"> ARE YOU AWARE?</span>
+</div>
+
+<div class="block3">
+<div class="bl1">
+<h1 >70%</h1>
+<p>of our debris sinks into the ocean's ecosystem</p>
+</div>
+<div class="bl1">
+<h1>88%</h1>
+<p>of the sea surface is polluted by plastic waste</p>
+</div>
+<div class="bl1">
+            <h1>30%</h1>
+            <p> of debris floats & lands on our beaches</p>
+ </div>
+<div class="bl3">
+<h1>100M</h1>
+<p>marine animals die each year due to plastic waste dumped in to the ocean</p>
+</div>
+</div>
+
+<div class="block4"> 
+<div class="box2">
+<p>
+The Damage We Caused So Far</br>NOT ANYMORE !</p>
+</div>
+</div>
+
+<div class="image4">
+<div class="b"></div>
+
+<div class="box3">
+<div class="b1">
+<div id="num">3x</div>
+<div id="txt">the size of </br> France</div>
+<p id="pp">The size of the "Great Pacific Garbage </br> Patch", a collection of floating plastic waste</p>
+</div>
+<div class="b2">
+<div id="num">9%</div>
+<p id="pp">The percentage of plastic that is recycled </br> each year</p>
+</div>
+<div class="b3">
+<div id="num">2050</div>
+<p id="pp">Year by which plastic will overweigh fish in </br> the ocean</p>
+</div>
+<div class="b4">
+<div id="num">150</div>
+<div id="txt">million </br> metric tons</div>
+<p id="pp">The amount of plastic waste in the ocean</p>
+</div>
+</div>
+</div>
+
+<div class="block5">
+<h1 style="font-family:ø;">SEDNA</h1>
+<div class="follow">
+<p>FOLLOW US ON</p>
+</br>
+<a id="ff" href="https://www.facebook.com/profile.php?id=100077396950173"><img src="fb.png" height="73px" width="73px"></a>
+</br></br>
+<a id="tt" href="https://twitter.com/Sedna2022"><img src="tw.png" height="60px" width="60px"></a>
+</div>
+<p id="address">COLOMBO, SRI LANKA</p>
+<p id="gmail">Hotline - <span id="tel">+94714596887</span> </br> sednalanka@gmail.com</p>
+
+<div class="policy">
+<hr>
+<ul><li><a href="terms.html">Terms & Conditions</a></li>
+<li><a href="donor policy.html">Donar Policy</a></li>
+<li>Contact Us</li>
+</ul>
+<ul class="faq">
+<li>FAQs</li>
+<li><a href="#top">Home</a></li>
+</ul>
+<hr>
+</div>
+<div class="copy">
+<ul>
+<li id="c">&copy 2022 Sedna</li>
+<li id="or">|</li>
+<li>www.sedna.com</li>
+</ul>
+</div>
+
+
+
+
+
+
+</body>
+
+</html>
